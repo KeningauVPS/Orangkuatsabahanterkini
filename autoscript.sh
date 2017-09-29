501f86c0ae648f9739e81830296eda58e90128f3
KeningauVPS <31593522+KeningauVPS@users.noreply.github.com>
2017-09-29 16:42:26 +0800

    Create autoscript.sh

---
+++ autoscript.sh
@@ -0,0 +1,7 @@
+wget -O menu https://github.com/arachi006/vps/raw/master/menu.sh
+chmod +x menu
+wget -O perbarui https://github.com/arachi006/vps/raw/master/daftar-menu.sh
+chmod +x perbarui
+perbarui
+cd
+menu
