633b6d8b5018b7032a8a09263ddfc3b9c9fd4ffd
KeningauVPS <31593522+KeningauVPS@users.noreply.github.com>
2017-09-29 16:32:32 +0800

    Create banned.sh

---
+++ banned.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+#Script Hapus User SSH
+read -p "Username yang akan diBANNED : " Login
+chage -E 0 $Login
+echo -e ".................................................."
+echo -e "User \e[1;33;44m$Login\e[0m Berhasil dibanned"
+echo -e "Script by \e[1;33;44m OrangKuatSabahanTerkini \e[0m"
